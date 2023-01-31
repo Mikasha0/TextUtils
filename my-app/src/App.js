27ab4eeb739be01826aa26c72796646e480fd343
Mikasha0 <anikettamrakar0@gmail.com>
2023-01-31 09:02:51 +0545

    removed unnecessary routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,18 +5,9 @@ import TextForm from "./components/TextForm";
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Footer from "./components/Footer";
-import Signin from "./components/Signin";
-// import Body from './components/Body';
-import Register from "./components/Register";
-import Errorpage from "./components/Errorpage";
-import FetchAPI from "./components/FetchAPI";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Login from "./components/Login";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-// import Signup from './components/signup';
-// import Trial from './components/trial';
 
 function App() {
   const [mode, setMode] = useState("dark");
@@ -67,7 +58,7 @@ function App() {
             toggleBtn={toggleMode}
           />
           <Alert alertMsg={alert} />
-
+          <TextForm btnMsg={showAlert} btnMode={mode} />
           <div className="container my-3">
             <Routes>
               <Route path="/about" element={<About />} />
@@ -75,30 +66,10 @@ function App() {
                 path="/home"
                 element={<TextForm btnMsg={showAlert} btnMode={mode} />}
               />
-              <Route
-                path="/register"
-                element={<Register btnName="Register" />}
-              />
-              <Route
-                path="*"
-                element={
-                  <Errorpage
-                    btn="Increase"
-                    btn2="Show Magic"
-                    btn3="Add toDoList"
-                  />
-                }
-              />
-              <Route path="/fetchAPI" element={<FetchAPI />} />
-              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
         </Router>
         <Footer btnMode={mode} />
-        <Signin />
-        {/* <div className='container mx-8'>
-        <Trial/>
-      </div> */}
       </QueryClientProvider>
     </>
   );
